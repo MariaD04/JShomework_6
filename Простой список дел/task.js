6f67d5dd7f42e6c73f98b4be0422d16b72f4d3d0
MariaD04 <117981238+MariaD04@users.noreply.github.com>
2023-11-17 14:04:01 +0400

    Update task.js

--- Простой список дел/task.js
+++ Простой список дел/task.js
@@ -1,27 +1,28 @@
 const tasksList = document.querySelector('.tasks__list');
+const button = document.querySelector('.tasks__add');
+const taskInput = document.getElementById('task__input');
 
-document.addEventListener('keydown', (event) => {  
-    if (event.key == 'Enter') {
-        let taskInput = (document.getElementById('task__input')).value;
+button.addEventListener('click', (event) => { 
+    event.preventDefault();
 
-        if (taskInput != '' && taskInput.trim() != '') {
-            let element = document.createElement('div');
-            element.className = 'task';
+    if (taskInput.value.trim() != '') {
+        let element = document.createElement('div');
+        element.className = 'task';
 
-            element.innerHTML += `
-                <div class="task__title">
-                ${taskInput}
-                </div>
-                <a href="#" class="task__remove">&times;</a>
-            `;
-            tasksList.insertAdjacentElement('afterbegin', element);
+        element.innerHTML += `
+            <div class="task__title">
+            ${taskInput.value}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+        `;
+        tasksList.insertAdjacentElement('afterbegin', element);
 
-            let link = element.querySelector('.task__remove');
-            link.addEventListener('click', () => {
-                let task = link.closest('div');
-                task.remove();
-            })
-        }
-        document.getElementById('task__input').value = '';
+        let link = element.querySelector('.task__remove');
+        link.addEventListener('click', () => {
+            let task = link.closest('div');
+            task.remove();
+        })
+    
+    taskInput.value = '';
     }   
 })
